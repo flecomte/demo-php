06036830cf2e4add87333153f785f37b81cfc9c6
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2020-10-22 23:55:29 +0200

    Add partition to the history table

--- Migration/src/Functions/history/insert.sql
+++ Migration/src/Functions/history/insert.sql
@@ -3,7 +3,8 @@ CREATE OR REPLACE FUNCTION insert_history (data json) RETURNS void
   PARALLEL SAFE
 AS $$
 BEGIN
-    INSERT INTO history (created_at, data)
-    SELECT (data->>'created_at')::timestamptz, data;
+    INSERT INTO history (id, created_at, data)
+    SELECT data->>'id', (data->>'created_at')::timestamptz, data
+    on conflict DO NOTHING;
 END;
 $$;
--- Migration/src/Migrations/2020-10/2020-10-22_17-29-47.up.sql
+++ Migration/src/Migrations/2020-10/2020-10-22_17-29-47.up.sql
@@ -1,6 +1,20 @@
 create table history
 (
-    id         uuid primary key default uuid_generate_v4(),
-    created_at timestamptz not null,
-    data       json        not null
-);
+    id         text,
+    created_at timestamptz,
+    data       json        not null,
+    primary key (created_at, id)
+) partition by RANGE (created_at);
+
+create table history_2010 partition of history for values from (MINVALUE) to (('2010-01-01'::timestamptz));
+
+do language plpgsql $$
+begin
+    for year in 2011..2024
+    loop
+        execute 'create table history_' || year || ' partition of history for values from ((''' || year || '-01-01''::timestamptz)) to ((''' || year+1 || '-01-01''::timestamptz))';
+    end loop;
+end;
+$$;
+
+create table history_2025 partition of history for values from (('2025-01-01'::timestamptz)) to (MAXVALUE);
